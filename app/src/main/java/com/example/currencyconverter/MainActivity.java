80d714f3a9e30cef6d4e56e765b3a648b63ce79e
Carl D. Bishop <48108505+UrthWyrm@users.noreply.github.com>
2021-04-07 16:05:46 -0700

    Finished

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,28 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Info", "Button Pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editTextCurrency);
+
+        String amountInYen = editText.getText().toString();
+
+        double amountInYenDouble = Double.parseDouble(amountInYen);
+
+        double amountInDollarsDouble = amountInYenDouble * 0.0091;
+
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+
+        Toast.makeText(this, "¥" + amountInYen + " is $" + amountInDollarsString , Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
